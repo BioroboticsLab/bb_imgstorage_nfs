9eddad5623dc6b11b422939fb04edf6861c1233e
David Dormagen <czapper@gmx.de>
2019-07-23 11:46:05 +0200

    fix typo

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ slack_channel = "#bb_notification"
 directory_watchdog_sleep_timer = 1.0 # in seconds.
 directory_watchdog_sleep_timer_on_error = 10.0 # in seconds.
 
-input_directory = "/mnt/local_storage/beesbook_2018/hd_recording/out"
+input_directory = "/mnt/local_storage/beesbook_2019/hd_recording/out"
 output_directory = "/mnt/zedat/beesbook/2019/hd_recording"
 directory_that_needs_to_be_a_mount_point = "/mnt/zedat"
 
